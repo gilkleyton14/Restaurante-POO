9e1f7f4c1e0e2daba0eea2977d6abb47f081c5ad
WellersonAlex <wellerson.alex@dce.ufpb.br>
2018-10-01 20:58:45 -0300

    Classe App

---
+++ App.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.poo.restaurantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App{
+	
+    public static void main( String[] args ){
+    	
+    	SpringApplication.run(App.class, args);
+    }
+}
